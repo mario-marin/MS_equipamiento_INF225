3a4bb30596f1c468b5deba38c1037d76e4763649
mario-marin <mario.marin.13@sansano.usm.cl>
2020-07-19 11:56:27 -0400

    initial commit of blank proyect

---
+++ MS-Equipamiento/src/main/java/com/MS_Equipamiento/MsEquipamientoApplication.java
@@ -0,0 +1,13 @@
+package com.MS_Equipamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEquipamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEquipamientoApplication.class, args);
+	}
+
+}
---
+++ MS-Equipamiento/src/test/java/com/MS_Equipamiento/MsEquipamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MS_Equipamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEquipamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
