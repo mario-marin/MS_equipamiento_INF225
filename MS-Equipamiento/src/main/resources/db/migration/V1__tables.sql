4d9ec82e3a0fc11681e02364e351812ba7fc4203
mario-marin <mario.marin.13@sansano.usm.cl>
2020-07-19 14:11:23 -0400

    added the ability to control migrations and added the defined tables

---
+++ MS-Equipamiento/src/main/resources/db/migration/V1__tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE Categoria (
+    IdCategoria UUID NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(256) NOT NULL,
+    Descripcion TEXT,
+    Estado INT NOT NULL
+);
+
+CREATE TABLE Equipamiento (
+    IdEquipamiento UUID NOT NULL PRIMARY KEY,
+    IdCategoria UUID NOT NULL,
+    Nombre VARCHAR(256) NOT NULL,
+    Descripcion TEXT,
+    Estado INT NOT NULL,
+    FOREIGN KEY (IdCategoria) REFERENCES Categoria(IdCategoria)
+);
+
+
+CREATE TABLE Log (
+    IdLog SERIAL PRIMARY KEY,
+    IdEquipamiento UUID,
+    IdCategoria UUID,
+    AccionRealizada INT NOT NULL,
+    Fecha INT NOT NULL,
+    FOREIGN KEY (IdEquipamiento) REFERENCES Equipamiento(IdEquipamiento),
+    FOREIGN KEY (IdCategoria) REFERENCES Categoria(IdCategoria)
+
+);
+
+CREATE TABLE Modificaciones (
+    IdModificacion UUID NOT NULL PRIMARY KEY,
+    IdLog INT NOT NULL,
+    AntesCambio TEXT NOT NULL,
+    DespuesCambio TEXT NOT NULL,
+    FOREIGN KEY (IdLog) REFERENCES Log(IdLog)
+
+);
+
